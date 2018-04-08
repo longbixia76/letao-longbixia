ed06d3fae1dc50cf05002abf0e6a6286b9db2ae0
longbixia76 <35287213+longbixia76@users.noreply.github.com>
2018-04-08 14:41:49 +0800

    首页静态页面、基本功能、头部、模态框、退出等基本功能完成

--- letao/public/back/js/common.js
+++ letao/public/back/js/common.js
@@ -22,3 +22,51 @@ $(document).ajaxStop(function () {
     },500)
 
 })
+
+
+//二级分类功能,点击分类管理，让下面的分类显示，再点击隐藏
+$(function(){
+    $(".category").click(function(){
+        //console.log(12312)
+        $(".child").stop().slideToggle();
+    })
+
+
+//点击左侧小图标菜单按钮，让侧边栏慢慢消失，让右侧的内容慢慢往左滑动
+    $(".icon_menu").click(function(){
+        //console.log('ifajdskf')
+        $(".aside").toggleClass("hidemenu");
+        $(".main").toggleClass("hidemenu");
+        $(".main-header").toggleClass("hidemenu");
+    })
+
+
+    $(".icon_logout").click(function(){
+        //console.log("jfsjkf")
+        $("#logoutModal").modal("show");
+    })
+    //要在外面给logoutBtn注册点击事件，避免重in复绑定事件，影响效率问题
+    $("#logoutBtn").click(function(){
+        //点击退出按钮，访问退出接口，发送ajax请求，
+        //console.log("点击事件")
+        $.ajax({
+            type:"GET",
+            url: "/employee/employeeLogout",
+            dataType:"json",
+            //data不需要传,因为接口不需要传数据
+            success: function(info){
+                //console.log(info)
+                if(info.success){
+                   //如果退出成功，需要跳转到登录页面
+                    location.href = "login.html"
+                }
+            }
+
+
+        })
+    })
+})
+
+//退出模态框
+
+
